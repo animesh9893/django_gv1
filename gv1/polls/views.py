6bc4c7545381ca332807286de592be4a6d573fa8
Animesh Shrivatri <animesh.shrivatri2002@gmail.com>
2021-03-06 13:02:14 +0530

    adding html page for index html iwth jinja and question

--- gv1/polls/views.py
+++ gv1/polls/views.py
@@ -5,8 +5,11 @@ from .models import Question
 
 def index(request):
 	latest_question_list=Question.objects.order_by("-pub_date")[:5] # minus for reverse of oldest list to recent 
-	output=', '.join([q.question_text for q in latest_question_list])
-	return HttpResponse(output)
+	# this dict is passed to html template
+	context={
+	'latest_question_list':latest_question_list,
+	}
+	return render(request,'polls/index.html',context)
 
 def detail(request,question_id):
 	return HttpResponse("You are looking at question %s" % question_id)
