bba16a48c45de1740ed36faeb27b3ce54ee50d92
Animesh Shrivatri <animesh.shrivatri2002@gmail.com>
2021-03-06 12:54:37 +0530

    using object orientation we get question from database and print to screen

--- gv1/polls/views.py
+++ gv1/polls/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Question
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello World you are in polls index")
+	latest_question_list=Question.objects.order_by("-pub_date")[:5] # minus for reverse of oldest list to recent 
+	output=', '.join([q.question_text for q in latest_question_list])
+	return HttpResponse(output)
+
+def detail(request,question_id):
+	return HttpResponse("You are looking at question %s" % question_id)
+
+def results(request,question_id):
+	response="You are looking at the results of question %s."
+	return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+	return HttpResponse("You are voting on question %s" % question_id)
